9146d77b8a866afb110af25fb26db075a2c5f5d4
Oskar <oskarradonkimball@gmail.com>
2021-02-07 13:53:06 -0800

    css grid implementation

--- site/snippets/exhibitions.php
+++ site/snippets/exhibitions.php
@@ -1,10 +1,23 @@
 <section id="exhibitions">
-  <h1><?= $data->title() ?></h1>
+  <article class="i-1">
+    <div class="indent"></div>
+    <div class="bracket">└</div>
+    <div class="content">
+      <h2><?= $data->title() ?></h2>
+    </div>
+  </article>
 
 
     <?php foreach($data->children()->listed() as $project): ?>
-      <p><?= $project->title() ?></p>
-      <p><?= $project->year() ?></p>
+      <article class="i-2">
+        <div class="indent"></div>
+        <div class="bracket">└</div>
+        <div class="content">
+          <h3><?= $project->title() ?></h3>
+          <p><?= $project->start()->toDate('F j, Y') ?> &ndash; <?= $project->end()->toDate('F j, Y') ?></p>
+          <?= $project->text()->kt() ?>
+        </div>
+      </article>
     <?php endforeach ?>
 
 </section>
