0c152f206854bb6f6829d289da9aa3e3c6842a15
Oskar <oskarradonkimball@gmail.com>
2021-02-07 15:28:11 -0800

    refactor to use ul and li elements instead of display:grid articles

--- site/snippets/exhibitions.php
+++ site/snippets/exhibitions.php
@@ -1,23 +1,13 @@
-<section id="exhibitions">
-  <article class="i-1">
-    <div class="indent"></div>
-    <div class="bracket">└</div>
-    <div class="content">
-      <h2><?= $data->title() ?></h2>
-    </div>
-  </article>
+<ul id="exhibitions">
+  <li class="i-1">
+    <h2><?= $data->title() ?></h2>
+  </li>
 
-
-    <?php foreach($data->children()->listed() as $project): ?>
-      <article class="i-2">
-        <div class="indent"></div>
-        <div class="bracket">└</div>
-        <div class="content">
-          <h3><?= $project->title() ?></h3>
-          <p><?= $project->start()->toDate('F j, Y') ?> &ndash; <?= $project->end()->toDate('F j, Y') ?></p>
-          <?= $project->text()->kt() ?>
-        </div>
-      </article>
-    <?php endforeach ?>
-
-</section>
+  <?php foreach($data->children()->listed() as $project): ?>
+    <li class="i-2">
+      <h3><?= $project->title() ?></h3>
+      <p><?= $project->start()->toDate('F j, Y') ?> &ndash; <?= $project->end()->toDate('F j, Y') ?></p>
+      <?= $project->text()->kt() ?>
+    </li>
+  <?php endforeach ?>
+</ul>
