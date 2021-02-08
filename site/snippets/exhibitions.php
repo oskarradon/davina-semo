a07b70c0f0bf1956f47313d3243f0dfc72b21010
Oskar <oskarradonkimball@gmail.com>
2021-02-07 20:50:34 -0800

    working exhibitions.php with elegant filtering

--- site/snippets/exhibitions.php
+++ site/snippets/exhibitions.php
@@ -1,52 +1,58 @@
-<ul id="currently">
+<?php
+  $current = $data
+    ->children()
+    ->filter(function ($child) {
+      return $child->start()->toDate() <= time() && $child->end()->toDate() >= time();
+    });
 
-  <?php
+  $past = $data
+    ->children()
+    ->filter(function ($child) {
+      return $child->start()->toDate() > time() || $child->end()->toDate() < time();
+    });
 
-    function isDateBetweenDates(DateTime $date, DateTime $startDate, DateTime $endDate) {
-        return $date > $startDate && $date < $endDate;
-    }
-
-    $currentTime = new DateTime();
-
-    foreach ($data->children()->listed() as $currentItem) :
-      $currentStart = DateTime::createFromFormat( 'U', $currentItem->start()->toDate() );
-      $currentEnd = DateTime::createFromFormat( 'U', $currentItem->end()->toDate() );
-      if (isDateBetweenDates($currentTime, $currentStart, $currentEnd) == true) :
-    ?>
+  if($current->isNotEmpty()):
+?>
+  <ul id="currently">
     <li class="i-1">Currently</li>
-        <li class="i-3">
-          <h3><?= $currentItem->title() ?></h3>
-          <p><?= $currentItem->start()->toDate('F j, Y') ?> &ndash; <?= $currentItem->end()->toDate('F j, Y') ?></p>
-          <?= $currentItem->text()->kt() ?>
-        </li>
-    <?php endif; ?>
-    <?php endforeach; ?>
+    <?php foreach($current as $e) : ?>
+      <li class="i-2">
+        <h3><?= $e->title() ?></h3>
+        <p><?= $e->start()->toDate('F j, Y') ?> &ndash; <?= $e->end()->toDate('F j, Y') ?></p>
+        <?= $e->text()->kt() ?>
+      </li>
+    <?php endforeach ?>
+  </ul>
+<?php endif ?>
 
-</ul>
+<?php if($past->isNotEmpty()): ?>
+  <ul id="exhibitions">
+    <li class="i-1"><?= $data->title() ?></li>
 
-<ul id="exhibitions">
-  <li class="i-1"><?= $data->title() ?></li>
+    <?php
 
-  <?php
+      // function that returns the formatted date
+      $callback = function($p) {
+        return $p->start()->date()->toDate('Y');
+      };
 
-    // function that returns the formatted date
-    $callback = function($p) {
-      return $p->start()->date()->toDate('Y');
-    };
-    // group items using $callback
-    $groupedItems = $data->children()->listed()->group($callback);
+      // group items using $callback
+      $groupedItems = $past->group($callback);
 
-    // output items by year
-    foreach($groupedItems as $year => $itemsPerYear): ?>
+      // output exhibitions by year
+      foreach($groupedItems as $year => $itemsPerYear):
+    ?>
       <li class="i-2">
         <?= $year ?>
       </li>
-        <?php foreach($itemsPerYear as $item) : ?>
-          <li class="i-3">
-            <h3><?= $item->title() ?></h3>
-            <p><?= $item->start()->toDate('F j, Y') ?> &ndash; <?= $item->end()->toDate('F j, Y') ?></p>
-            <?= $item->text()->kt() ?>
-          </li>
-        <?php endforeach; ?>
-  <?php endforeach; ?>
-</ul>
+      <?php foreach($itemsPerYear as $item) : ?>
+        <li class="i-3">
+          <?php time() ?>
+          <h3><?= $item->title() ?></h3>
+          <p><?= $item->start()->toDate('F j, Y') ?> &ndash; <?= $item->end()->toDate('F j, Y') ?></p>
+          <?= $item->text()->kt() ?>
+        </li>
+      <?php endforeach; ?>
+    <?php endforeach; ?>
+  </ul>
+<?php endif ?>
