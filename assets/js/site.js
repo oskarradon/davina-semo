0de81833b75d408f8d0436a72150cc22531e1303
Oskar <oskarradonkimball@gmail.com>
2021-02-08 12:10:04 -0800

    nested lists idea, not working

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,16 @@
-console.log("hello :)")
+document.querySelectorAll('ul').forEach((el) => {
+  var iL = el.classList[0].replace('i-','');
+
+
+  el.addEventListener('click', (v) => {
+    v.preventDefault();
+    console.log(iL);
+    const listItems = el.children;
+    console.log(listItems)
+    const listArray = Array.from(el.children);
+    listArray.forEach((item) => {
+      console.log(item)
+      item.classList.toggle('hidden');
+    });
+  });
+});
