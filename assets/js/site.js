5380333aa9bcc1d38ea05aa7e195f06081cedac1
Oskar <oskarradonkimball@gmail.com>
2021-02-08 12:20:54 -0800

    function returns sibling elements

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,16 +1,40 @@
-document.querySelectorAll('ul').forEach((el) => {
-  var iL = el.classList[0].replace('i-','');
+document.querySelectorAll('ul li').forEach((el) => {
+  // indentLevel variable
+  var iL = parseInt(el.classList[0].replace('i-',''));
 
 
   el.addEventListener('click', (v) => {
     v.preventDefault();
     console.log(iL);
-    const listItems = el.children;
-    console.log(listItems)
-    const listArray = Array.from(el.children);
-    listArray.forEach((item) => {
-      console.log(item)
-      item.classList.toggle('hidden');
-    });
+    console.log(typeof iL)
+
+    var getSiblings = function (elem) {
+
+    	// Setup siblings array and get the first sibling
+    	var siblings = [];
+    	var sibling = elem.parentNode.firstChild;
+
+    	// Loop through each sibling and push to the array
+    	while (sibling) {
+    		if (sibling.nodeType === 1 && sibling !== elem) {
+    			siblings.push(sibling);
+    		}
+    		sibling = sibling.nextSibling
+    	}
+
+    	return siblings;
+
+    };
+
+    var x = getSiblings(el);
+    console.log(x)
+    // const listArray = Array.from(el.children);
+    // listArray.forEach((item) => {
+    //   console.log(item)
+    //   item.classList.toggle('hidden');
+    // });
   });
 });
+
+// when you click on an element with class list i-NUMBER
+// show all sibling elements with class i-NUMBER++
--- site/snippets/exhibitions.php
+++ site/snippets/exhibitions.php
@@ -13,50 +13,46 @@
 
   if($current->isNotEmpty()):
 ?>
-  <ul class="i-1" id="currently">
-    <li>Currently
-      <ul class="i-2">
-        <?php foreach($current as $e) : ?>
-          <li>
-            <h3><?= $e->title() ?></h3>
-            <p><?= $e->start()->toDate('F j, Y') ?> &ndash; <?= $e->end()->toDate('F j, Y') ?></p>
-            <?= $e->text()->kt() ?>
-          </li>
-        <?php endforeach ?>
-      </ul>
-    </li>
+  <ul id="currently">
+    <li class="i-1">Currently</li>
+    <?php foreach($current as $e) : ?>
+      <li class="i-2">
+        <h3><?= $e->title() ?></h3>
+        <p><?= $e->start()->toDate('F j, Y') ?> &ndash; <?= $e->end()->toDate('F j, Y') ?></p>
+        <?= $e->text()->kt() ?>
+      </li>
+    <?php endforeach ?>
   </ul>
 <?php endif ?>
 
 <?php if($past->isNotEmpty()): ?>
-  <ul class="i-1" id="exhibitions">
-    <li>
-      <?= $data->title() ?>
-        <?php
-          // function that returns the formatted date
-          $callback = function($p) {
-            return $p->start()->date()->toDate('Y');
-          };
-          // group items using $callback
-          $groupedItems = $past->group($callback);
-          // output exhibitions by year
-          foreach($groupedItems as $year => $itemsPerYear):
-        ?>
-        <ul class="i-2">
-        <li>
-          <?= $year ?>
-            <?php foreach($itemsPerYear as $item) : ?>
-              <ul class="i-3">
-                <li>
-                  <h3><?= $item->title() ?></h3>
-                  <p><?= $item->start()->toDate('F j, Y') ?> &ndash; <?= $item->end()->toDate('F j, Y') ?></p>
-                  <?= $item->text()->kt() ?>
-                </li>
-              </ul>
-            <?php endforeach; ?>
+  <ul id="exhibitions">
+    <li class="i-1"><?= $data->title() ?></li>
+
+    <?php
+
+      // function that returns the formatted date
+      $callback = function($p) {
+        return $p->start()->date()->toDate('Y');
+      };
+
+      // group items using $callback
+      $groupedItems = $past->group($callback);
+
+      // output exhibitions by year
+      foreach($groupedItems as $year => $itemsPerYear):
+    ?>
+      <li class="i-2">
+        <?= $year ?>
+      </li>
+      <?php foreach($itemsPerYear as $item) : ?>
+        <li class="i-3">
+          <?php time() ?>
+          <h3><?= $item->title() ?></h3>
+          <p><?= $item->start()->toDate('F j, Y') ?> &ndash; <?= $item->end()->toDate('F j, Y') ?></p>
+          <?= $item->text()->kt() ?>
         </li>
-      </ul>
+      <?php endforeach; ?>
     <?php endforeach; ?>
-    </li>
   </ul>
 <?php endif ?>
