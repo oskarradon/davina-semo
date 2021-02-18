6927e93af18066b16a29f157561acce8084cdaab
Oskar <oskarradonkimball@gmail.com>
2021-02-18 15:57:06 -0800

    test is working correctly

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,19 +1,27 @@
-var zoomImage = document.querySelector('#main-image img');
-var slider = document.getElementById('slider');
-slider.addEventListener('input', function() {
-    zoomImage.style.width = slider.value+'%';
-}, false);
-
+// * * * * * *
+// EXPANDABLE DATA TREE
+// * * * * * *
 
 var coll = document.getElementsByClassName("collapsible");
 
-//setting buttons of class 'collapsible' to extend on click
+//set elements with class 'collapsible' to extend on click
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
     mainImgToggle()
   });
 }
+
+
+// * * * * * *
+// MAIN IMAGE
+// * * * * * *
+
+var zoomImage = document.querySelector('#main-image img');
+var slider = document.getElementById('slider');
+slider.addEventListener('input', function() {
+    zoomImage.style.width = slider.value+'%';
+}, false);
 
 // hides main img & slider if any 1st-level elements are expanded
 function mainImgToggle() {
@@ -22,7 +30,7 @@ function mainImgToggle() {
     if (elements[i].classList.contains("active")) {
       zoomImage.classList.add("hide")
       slider.classList.add("hide")
-      break
+      break // important to exit loop once if statement evaluates to true the first time
     } else {
       zoomImage.classList.remove("hide")
       slider.classList.remove("hide")
@@ -30,6 +38,25 @@ function mainImgToggle() {
   }
 }
 
-const lightbox = GLightbox({
-  width: "100vw"
+
+// * * * * *
+// LIGHTBOX
+// * * * * *
+
+const lightbox = GLightbox();
+
+document.querySelector('.asdf').addEventListener("click", function () {
+  console.log("fgff")
+  lightbox.open(document.querySelector('figure a[data-gallery=gallery1]'))
 });
+
+// for every div with class .content
+// that contains div with class .gallery
+
+// attach click event handler to .i-2
+// then open lightbox with gallery number == $showCount
+
+// schema:
+// div.i-2
+//   div.gallery
+//     figure a[data-gallery=galleryX]  <-- X = $showCount
--- site/snippets/exhibitions.php
+++ site/snippets/exhibitions.php
@@ -17,41 +17,46 @@
   <section id="currently">
     <div class="i i-1 collapsible"><h2>Currently</h2></div>
     <div class="content">
-      <?php foreach($current as $e) : ?>
+      <?php foreach($current as $e) :
+              static $showCount = 0;
+              $showCount++; ?>
 
-        <div class="i i-2">
+        <div class="i i-2 asdf">
           <h3><?= $e->title() ?></h3>
           <p><?= $e->start()->toDate('F j, Y') ?> &ndash; <?= $e->end()->toDate('F j, Y') ?></p>
           <?= $e->text()->kt() ?>
         </div>
 
-        <div class="gallery">
-          <?php foreach($e->images() as $i) :
-            static $imageCount = 0;
-            $imageCount++; ?>
-            <figure>
-              <a href="<?= $i->url() ?>"
-                 class="glightbox"
-                 data-gallery="gallery1"
-                 data-glightbox="description: .custom-desc<?php echo $imageCount ?>">
-                <img src="<?= $i->url() ?>" alt="">
-              </a>
-              <figcaption class="glightbox-desc
-                                 custom-desc<?php echo $imageCount ?>">
-                <p><?= $i->description() ?></p>
-                <span><?php echo $imageCount ?> / <?= $e->images()->count() ?></span>
-              </figcaption>
-            </figure>
-          <?php endforeach ?>
-        </div>
+        <?php if($e->images()) :
+              // only show gallery el if exhibition has imgs ?>
+          <div class="gallery">
+            <?php foreach($e->images() as $i) :
+                    static $imageCount = 0;
+                    $imageCount++; ?>
+              <figure>
+                <a href="<?= $i->url() ?>"
+                   class="glightbox"
+                   data-gallery="gallery<?php echo $showCount ?>"
+                   data-glightbox="description: .custom-desc<?php echo $imageCount ?>">
+                  <img src="<?= $i->url() ?>" alt="">
+                </a>
+                <figcaption class="glightbox-desc
+                                   custom-desc<?php echo $imageCount ?>">
+                  <p><?= $i->description() ?></p>
+                  <span><?php echo $imageCount ?> / <?= $e->images()->count() ?></span>
+                </figcaption>
+              </figure>
+            <?php endforeach ?>
+          </div>
+        <?php endif ?>
 
-          <?php foreach($e->documents() as $d) : ?>
-            <div class="i i-3">
-              <a href="<?= $d->url() ?>"><?= $d->description() ?></a>
-            </div>
-          <?php endforeach ?>
+        <?php foreach($e->documents() as $d) : ?>
+          <div class="i i-3">
+            <a href="<?= $d->url() ?>"><?= $d->description() ?></a>
+          </div>
+        <?php endforeach ?>
 
-          <?php if($e->link()) : ?>
+        <?php if($e->link()) : ?>
           <div class="i i-3">
             <a href="<?= $e->link()->url() ?>">View documentation</a>
           </div>
