7eea35548896fa58d9d5e8cec1bdaf65bbe704d4
Oskar <oskarradonkimball@gmail.com>
2021-03-05 15:51:26 -0800

    format exhibitions to match currently

--- site/snippets/exhibitions.php
+++ site/snippets/exhibitions.php
@@ -17,57 +17,58 @@
   <section id="currently">
     <div class="i i-1 collapsible"><h2>Currently</h2></div>
     <div class="content">
-      <?php foreach($current as $e) :
-              static $showCount = 0;
-              $showCount++; ?>
+      <?php foreach($current as $c) :
+              static $currentCount = 0;
+                     $currentCount++; ?>
+        <article>
+          <div class="i i-2">
+            <h3><?= $c->title() ?></h3>
+            <p><?= $c->start()->toDate('F j, Y') ?> &ndash; <?= $c->end()->toDate('F j, Y') ?></p>
+            <?= $c->text()->kt() ?>
 
-        <div class="i i-2">
-          <h3><?= $e->title() ?></h3>
-          <p><?= $e->start()->toDate('F j, Y') ?> &ndash; <?= $e->end()->toDate('F j, Y') ?></p>
-          <?= $e->text()->kt() ?>
 
-
-          <?php if($e->images()->isNotEmpty()) :
-                // only show gallery el if exhibition has imgs ?>
-            <div class="gallery">
-              <?php
-              $images = $e->images();
-              $total = $images->count();
-              foreach($images as $i) :
-                      static $imageCount = 0;
-                      $imageCount++; ?>
-                <figure>
-                  <a href="<?= $i->url() ?>"
-                     class="glightbox"
-                     data-gallery="gallery<?php echo $showCount ?>"
-                     data-glightbox="description: .custom-desc<?php echo $imageCount ?>">
-                    <img src="<?= $i->url() ?>" alt="">
-                  </a>
-                  <figcaption class="glightbox-desc
-                                     custom-desc<?php echo $imageCount ?>">
-                    <p><?= $i->description() ?></p>
+            <?php if($c->images()->isNotEmpty()) :
+              // only show gallery el if exhibition has imgs ?>
+              <div class="gallery">
+                <?php
+                $currentImages = $c->images();
+                $currentTotal = $currentImages->count();
+                foreach($currentImages as $cI) :
+                  static $currentImageCount = 0;
+                  $currentImageCount++; ?>
+                  <figure>
+                    <a href="<?= $cI->url() ?>"
+                      class="glightbox"
+                      data-gallery="gallery<?php echo $currentCount ?>"
+                      data-glightbox="description: .custom-desc<?php echo $currentImageCount ?>">
+                      <img src="<?= $cI->url() ?>" alt="">
+                    </a>
+                    <figcaption class="glightbox-desc
+                    custom-desc<?php echo $currentImageCount ?>">
+                    <p><?= $cI->description() ?></p>
                     <div class="image-counter">
-                       <?= $images->indexOf($i) + 1 . "/" . $total;?>
-                     </div>
+                      <?= $currentImages->indexOf($cI) + 1 . "/" . $currentTotal;?>
+                    </div>
                   </figcaption>
                 </figure>
               <?php endforeach ?>
             </div>
+            <?php endif ?>
+
+          </div>
+
+          <?php foreach($c->documents() as $cD) : ?>
+            <div class="i i-3">
+              <a href="<?= $cD->url() ?>"><?= $cD->description() ?></a>
+            </div>
+          <?php endforeach ?>
+
+          <?php if($c->link()->isNotEmpty()) : ?>
+            <div class="i i-3">
+              <a href="<?= $c->link()->url() ?>">View documentation</a>
+            </div>
           <?php endif ?>
-
-        </div>
-
-        <?php foreach($e->documents() as $d) : ?>
-          <div class="i i-3">
-            <a href="<?= $d->url() ?>"><?= $d->description() ?></a>
-          </div>
-        <?php endforeach ?>
-
-        <?php if($e->link()->isNotEmpty()) : ?>
-          <div class="i i-3">
-            <a href="<?= $e->link()->url() ?>">View documentation</a>
-          </div>
-        <?php endif ?>
+        </article>
 
       <?php endforeach ?>
     </div>
@@ -87,20 +88,66 @@
           return $p->start()->date()->toDate('Y');
         };
         // group items using $callback
-        $groupedItems = $past->group($callback);
+        $groupedExhibitions = $past->group($callback);
         // output exhibitions by year
-        foreach($groupedItems as $year => $itemsPerYear):
+        foreach($groupedExhibitions as $year => $exhibitionsPerYear):
       ?>
-        <div class="i i-2 collapsible"><?= $year ?></div>
+        <div class="i i-2 collapsible"><h2><?= $year ?></h2></div>
         <div class="content">
-          <?php foreach($itemsPerYear as $item) : ?>
-            <div class="i i-3">
-              <h3 class="collapsible"><?= $item->title() ?></h3>
-              <div class="content">
-                <p><?= $item->start()->toDate('F j, Y') ?> &ndash; <?= $item->end()->toDate('F j, Y') ?></p>
-                <?= $item->text()->kt() ?>
+          <?php foreach($exhibitionsPerYear as $e) :
+                 static $exhibitionCount = 0;
+                        $exhibitionCount++; ?>
+
+            <article>
+              <div class="i i-3">
+                <h3><?= $e->title() ?></h3>
+                <p><?= $e->start()->toDate('F j, Y') ?> &ndash; <?= $e->end()->toDate('F j, Y') ?></p>
+                <?= $e->text()->kt() ?>
+
+
+                <?php if($e->images()->isNotEmpty()) :
+                  // only show gallery el if exhibition has imgs ?>
+                  <div class="gallery">
+                    <?php
+                    $exhibitionImages = $e->images();
+                    $exhibitionTotal = $exhibitionImages->count();
+                    foreach($exhibitionImages as $eI) :
+                      static $exhibitionImageCount = 0;
+                      $exhibitionImageCount++; ?>
+                      <figure>
+                        <a href="<?= $eI->url() ?>"
+                          class="glightbox"
+                          data-gallery="gallery<?php echo $exhibitionCount ?>"
+                          data-glightbox="description: .custom-desc<?php echo $exhibitionImageCount ?>">
+                          <img src="<?= $eI->url() ?>" alt="">
+                        </a>
+                        <figcaption class="glightbox-desc
+                        custom-desc<?php echo $exhibitionImageCount ?>">
+                        <p><?= $eI->description() ?></p>
+                        <div class="image-counter">
+                          <?= $exhibitionImages->indexOf($eI) + 1 . "/" . $exhibitionTotal;?>
+                        </div>
+                      </figcaption>
+                    </figure>
+                  <?php endforeach ?>
+                </div>
+                <?php endif ?>
+
               </div>
-            </div>
+
+              <?php foreach($e->documents() as $eD) : ?>
+                <div class="i i-4">
+                  <a href="<?= $eD->url() ?>"><?= $eD->description() ?></a>
+                </div>
+              <?php endforeach ?>
+
+              <?php if($e->link()->isNotEmpty()) : ?>
+                <div class="i i-4">
+                  <a href="<?= $e->link()->url() ?>">View documentation</a>
+                </div>
+              <?php endif ?>
+            </article>
+
           <?php endforeach; ?>
         </div>
       <?php endforeach; ?>
