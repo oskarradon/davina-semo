520248786d6a32c4e923840ee63e93f4775714e8
Oskar <oskarradonkimball@gmail.com>
2021-03-10 11:43:43 -0800

    Create representation.php

---
+++ site/snippets/representation.php
@@ -0,0 +1,20 @@
+<?php
+  // filtered collection of all current exhibitions
+  $galleries = $data->children()->listed();
+  // only show "currently" section if there are pages that match above criteria
+  if($galleries->isNotEmpty()):
+?>
+  <section id="galleries">
+    <div class="i i-1 collapsible"><h2>Selected Press</h2></div>
+    <div class="content">
+      <?php foreach($galleries as $g) : ?>
+
+        <a class="i i-2" href="<?= $g->link() ?>">
+          <h3><?= $g->title() ?></h3>
+          <?= $g->description()->kt() ?>
+        </a>
+
+      <?php endforeach ?>
+    </div>
+  </section>
+<?php endif ?>
