77ba12c251d1ed03e38104357eb6664f0520718d
Oskar <oskarradonkimball@gmail.com>
2021-03-21 15:03:51 -0700

    Update exhibitions.php

--- site/snippets/exhibitions.php
+++ site/snippets/exhibitions.php
@@ -14,6 +14,7 @@
   // only show "currently" section if there are pages that match above criteria
   if($current->isNotEmpty()):
 ?>
+
   <section id="currently">
     <div class="i i-1 collapsible"><h2>Currently</h2></div>
     <div class="content">
@@ -22,28 +23,26 @@
           <div class="i i-2 galleryToggle toggle<?= $c->uid() ?>">
             <h3><?= $c->title() ?></h3>
             <p><?= $c->start()->toDate('F j, Y') ?> &ndash; <?= $c->end()->toDate('F j, Y') ?></p>
-            <?= $c->text()->kt() ?>
+            <?= $c->description()->kt() ?>
 
 
-            <?php if($c->images()->isNotEmpty()) :
-              // only show gallery el if exhibition has imgs ?>
-              <div class="preview-image"><?= $c->images()->first() ?></div>
+            <?php if($c->images()->isNotEmpty()) : ?>
               <div class="gallery">
                 <?php
-                $currentImages = $c->images();
+                $currentImages = $c->images()->sortBy('sort');
                 $currentTotal = $currentImages->count();
                 foreach($currentImages as $cI) :
                   static $currentImageCount = 0;
-                  $currentImageCount++; ?>
+                         $currentImageCount++; ?>
                   <figure>
                     <a href="<?= $cI->url() ?>"
                       class="glightbox"
                       data-gallery="gallery<?= $c->uid() ?>"
-                      data-glightbox="description: .custom-desc<?php echo $currentImageCount ?>">
+                      data-glightbox="description: .custom-desc<?= $c->uid() ?><?php echo $currentImageCount ?>">
                       <img src="<?= $cI->url() ?>" alt="">
                     </a>
                     <figcaption class="glightbox-desc
-                    custom-desc<?php echo $currentImageCount ?>">
+                    custom-desc<?= $c->uid() ?><?php echo $currentImageCount ?>">
                     <p><?= $cI->description() ?></p>
                     <p class="image-counter">
                       <?= $currentImages->indexOf($cI) + 1 . "/" . $currentTotal;?>
@@ -52,9 +51,11 @@
                 </figure>
               <?php endforeach ?>
             </div>
-            <?php endif ?>
 
           </div>
+
+          <div class="preview-image"><?= $currentImages->first() ?></div>
+        <?php endif ?>
 
           <?php foreach($c->documents() as $cD) : ?>
             <div class="i i-3">
@@ -67,6 +68,7 @@
               <a href="<?= $c->link()->url() ?>" target="_blank">View documentation</a>
             </div>
           <?php endif ?>
+
         </article>
 
       <?php endforeach ?>
@@ -83,57 +85,53 @@
     <div class="content">
       <?php
         // returns the year of an exhibition
-        $callback = function($p) {
+        $returnYear = function($p) {
           return $p->start()->date()->toDate('Y');
         };
         // group items using $callback
-        $groupedExhibitions = $past->group($callback);
+        $groupedExhibitions = $past->group($returnYear);
         // output exhibitions by year
         foreach($groupedExhibitions as $year => $exhibitionsPerYear):
       ?>
         <div class="i i-2 collapsible"><h2><?= $year ?></h2></div>
         <div class="content">
-          <?php foreach($exhibitionsPerYear as $e) :
-                 static $exhibitionCount = 0;
-                        $exhibitionCount++; ?>
-
+          <?php foreach($exhibitionsPerYear as $e) : ?>
             <article>
               <div class="i i-3 galleryToggle toggle<?= $e->uid() ?>">
                 <h3><?= $e->title() ?></h3>
                 <p><?= $e->start()->toDate('F j, Y') ?> &ndash; <?= $e->end()->toDate('F j, Y') ?></p>
-                <?= $e->text()->kt() ?>
+                <?= $e->description()->kt() ?>
 
-
-                <?php if($e->images()->isNotEmpty()) :
-                  // only show gallery el if exhibition has imgs ?>
-                  <div class="preview-image"><?= $e->images()->first() ?></div>
+                <?php if($e->images()->isNotEmpty()) : ?>
                   <div class="gallery">
                     <?php
-                    $exhibitionImages = $e->images();
+                    $exhibitionImages = $e->images()->sortBy('sort');
                     $exhibitionTotal = $exhibitionImages->count();
                     foreach($exhibitionImages as $eI) :
                       static $exhibitionImageCount = 0;
-                      $exhibitionImageCount++; ?>
+                             $exhibitionImageCount++; ?>
                       <figure>
                         <a href="<?= $eI->url() ?>"
                           class="glightbox"
                           data-gallery="gallery<?= $e->uid() ?>"
-                          data-glightbox="description: .custom-desc<?php echo $exhibitionImageCount ?>">
+                          data-glightbox="description: .custom-desc<?= $e->uid() ?><?php echo $exhibitionImageCount ?>">
                           <img src="<?= $eI->url() ?>" alt="">
                         </a>
                         <figcaption class="glightbox-desc
-                        custom-desc<?php echo $exhibitionImageCount ?>">
-                        <p><?= $eI->description() ?></p>
-                        <p class="image-counter">
-                          <?= $exhibitionImages->indexOf($eI) + 1 . "/" . $exhibitionTotal;?>
-                        </p>
-                      </figcaption>
-                    </figure>
-                  <?php endforeach ?>
-                </div>
-                <?php endif ?>
+                        custom-desc<?= $e->uid() ?><?php echo $exhibitionImageCount ?>">
+                          <p><?= $eI->description() ?></p>
+                          <p class="image-counter">
+                            <?= $exhibitionImages->indexOf($eI) + 1 . "/" . $exhibitionTotal;?>
+                          </p>
+                        </figcaption>
+                      </figure>
+                    <?php endforeach ?>
+                  </div>
+                  </div>
 
-              </div>
+                  <div class="preview-image"><?= $exhibitionImages->first() ?></div>
+
+                <?php endif ?>
 
               <?php foreach($e->documents() as $eD) : ?>
                 <div class="i i-4">
@@ -150,7 +148,7 @@
 
           <?php endforeach; ?>
         </div>
+
       <?php endforeach; ?>
-    </div>
   </section>
 <?php endif ?>
