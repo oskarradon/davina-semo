7865d8b9d51a972db40cbb77db7a31ae50fa6139
Oskar <oskarradonkimball@gmail.com>
2021-03-21 15:03:56 -0700

    Update representation.php

--- site/snippets/representation.php
+++ site/snippets/representation.php
@@ -5,7 +5,7 @@
   if($galleries->isNotEmpty()):
 ?>
   <section id="galleries">
-    <div class="i i-1 collapsible"><h2>Selected Press</h2></div>
+    <div class="i i-1 collapsible"><h2><?= $data->title() ?></h2></div>
     <div class="content">
       <?php foreach($galleries as $g) : ?>
 
