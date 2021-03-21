7ad83ee11acf4429166bd412c8eae699fa605e89
Oskar <oskarradonkimball@gmail.com>
2021-03-21 15:03:59 -0700

    Update home.php

--- site/templates/home.php
+++ site/templates/home.php
@@ -8,6 +8,24 @@
 
 <body>
 
+    <!-- main image, preview image, and model viewer -->
+
+    <?php
+      $file = $site->files()->sortBy('sort')->first();
+      if($file->extension() == 'glb') : ?>
+      <model-viewer id="main-image" src="<?= $file->url() ?>" auto-rotate camera-controls>
+        <div class="progress-bar hide" slot="progress-bar">
+            <div class="update-bar"></div>
+        </div>
+      </model-viewer>
+
+    <?php else : ?>
+      <div id="main-image"><?= $file ?></div>
+      <aside>
+        <input id="slider" type="range" min="1" max="200">
+      </aside>
+    <?php endif ?>
+
   <main>
 
       <h1><?= $site->title() ?></h1>
@@ -20,24 +38,6 @@
       ?>
 
   </main>
-
-  <!-- main image, preview image, and model viewer -->
-
-  <?php
-    $file = $site->files()->sortBy('sort')->first();
-    if($file->extension() == 'glb') : ?>
-    <model-viewer id="main-image" src="<?= $file->url() ?>" auto-rotate camera-controls>
-      <div class="progress-bar hide" slot="progress-bar">
-          <div class="update-bar"></div>
-      </div>
-    </model-viewer>
-
-  <?php else : ?>
-    <div id="main-image"><?= $file ?></div>
-    <aside>
-      <input id="slider" type="range" min="1" max="200">
-    </aside>
-  <?php endif ?>
 
 </body>
 
