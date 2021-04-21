1b9d3853b704caffda1b7dd0bb6e6310ef1befab
Oskar <oskarradonkimball@gmail.com>
2021-04-21 11:33:37 -0700

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,53 +2,63 @@
 // EXPANDABLE TREE
 // * * * * * * * *
 
-for (const collapsible of document.querySelectorAll('.collapsible')) {
-  collapsible.addEventListener('click', function () {
-    this.classList.toggle('active');
+for (const collapsible of document.querySelectorAll(".collapsible")) {
+  collapsible.addEventListener("click", function () {
+    this.classList.toggle("active");
     mainImgToggle();
   });
 }
+
+document.getElementById("logo").addEventListener("click", function () {
+  for (const collapsible of document.querySelectorAll(".collapsible")) {
+    collapsible.classList.remove("active");
+  }
+});
 
 // * * * * * *
 // MAIN IMAGE
 // * * * * * *
 
-let mainImage = document.querySelector('div#main-image img');
+let mainImage = document.querySelector("div#main-image img");
 
 // image zoom slider
 
-let slider = document.getElementById('slider');
+let slider = document.getElementById("slider");
 
 if (slider) {
-  mainImage.style.height = slider.value + '%';
-  slider.addEventListener('input', function () {
-      mainImage.style.height = slider.value + '%';
-    }, false);
+  mainImage.style.height = slider.value + "%";
+  slider.addEventListener(
+    "input",
+    function () {
+      mainImage.style.height = slider.value + "%";
+    },
+    false
+  );
 }
 
 // hides main img/model-viewer & slider if any 1st-level elements are expanded
 function mainImgToggle() {
-  let elements = document.getElementsByClassName('i-1');
-  let modelViewer = document.querySelector('model-viewer');
+  let elements = document.getElementsByClassName("i-1");
+  let modelViewer = document.querySelector("model-viewer");
 
   if (modelViewer) {
     for (let i = 0; i < elements.length; i++) {
-      if (elements[i].classList.contains('active')) {
-        modelViewer.classList.add('hide');
+      if (elements[i].classList.contains("active")) {
+        modelViewer.classList.add("hide");
         break; // important to exit loop once if statement evaluates to true the first time
       } else {
-        modelViewer.classList.remove('hide');
+        modelViewer.classList.remove("hide");
       }
     }
   } else if (mainImage) {
     for (let i = 0; i < elements.length; i++) {
-      if (elements[i].classList.contains('active')) {
-        mainImage.classList.add('hide');
-        slider.classList.add('hide');
+      if (elements[i].classList.contains("active")) {
+        mainImage.classList.add("hide");
+        slider.classList.add("hide");
         break;
       } else {
-        mainImage.classList.remove('hide');
-        slider.classList.remove('hide');
+        mainImage.classList.remove("hide");
+        slider.classList.remove("hide");
       }
     }
   }
@@ -74,17 +84,18 @@ const customLightboxHTML = `<div id="glightbox-body" class="glightbox-container"
 const lightbox = GLightbox({
   lightboxHTML: customLightboxHTML,
   zoomable: false,
+  autoplayVideos: true,
 });
 
 // if .galleryToggle contains a gallery,
 // open the corresponding lightbox on click
 
-for (const toggle of document.querySelectorAll('.galleryToggle')) {
-  let hasGallery = toggle.querySelector('.gallery') != null;
-  let  galleryId  = toggle.classList[3].replace('toggle', '');
-  let gallerySelector = 'figure a[data-gallery=gallery'.concat(galleryId);
+for (const toggle of document.querySelectorAll(".galleryToggle")) {
+  let hasGallery = toggle.querySelector(".gallery") != null;
+  let galleryId = toggle.classList[3].replace("toggle", "");
+  let gallerySelector = "figure a[data-gallery=gallery".concat(galleryId);
   if (hasGallery) {
-    toggle.addEventListener('click', function () {
+    toggle.addEventListener("click", function () {
       lightbox.open(document.querySelector(gallerySelector));
     });
   }
