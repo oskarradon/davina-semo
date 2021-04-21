3297bee6b6c5912b49f1dcd3a7a2cd1d2a2157f3
Oskar <oskarradonkimball@gmail.com>
2021-04-21 11:36:12 -0700

    Update article.php

--- site/snippets/article.php
+++ site/snippets/article.php
@@ -1,29 +1,39 @@
 <article>
-  <div class="i i-2 galleryToggle toggle<?= $article->uid() ?>">
+  <div class="i i-4 galleryToggle toggle<?= $article->uid() ?>">
     <h3><?= $article->title() ?></h3>
     <p><?= $article->start()->toDate('F j, Y') ?> &ndash; <?= $article->end()->toDate('F j, Y') ?></p>
     <?= $article->description()->kt() ?>
 
-    <?php if($article->images()->isNotEmpty()) : ?>
+    <?php 
+    $c = $article->files()->template("carousel");
+    if($c->isNotEmpty()) : ?>
       <div class="gallery">
         <?php
-        $imgs = $article->images()->sortBy('sort');
-        $imgCount = $imgs->count();
-        foreach($imgs as $cI) :
+        $files = $c->sortBy('sort');
+        $fileCount = $files->count();
+        foreach($files as $file) :
           static $currentImageCount = 0;
-                  $currentImageCount++; ?>
+                 $currentImageCount++; ?>
           <figure>
-            <a href="<?= $cI->url() ?>"
+            <a href="<?= $file->url() ?>"
               class="glightbox"
               data-gallery="gallery<?= $article->uid() ?>"
               data-glightbox="description: .custom-desc<?= $article->uid() ?><?php echo $currentImageCount ?>">
-              <img src="<?= $cI->url() ?>" alt="">
+            <?php if($file->type() == 'image') : ?>
+              <img src="<?= $file->url() ?>" alt="" <?php if ($file->orientation() == 'portrait') : ?> class="portrait" <?php endif ?> />
+            <?php elseif($file->type() == 'video') : ?>
+              <video playsinline autoplay muted loop>
+                <source src="<?= $file->url() ?>" type="<?= $file->mime() ?>">
+              </video>
+            <?php else : ?>
+              <audio controls src="<?= $file->url() ?>" type="<?= $file->mime() ?>"></audio>
+            <?php endif ?>
             </a>
             <figcaption class="glightbox-desc
             custom-desc<?= $article->uid() ?><?php echo $currentImageCount ?>">
-            <p><?= $cI->description() ?></p>
+            <p><?= $file->description() ?></p>
             <p class="image-counter">
-              <?= $imgs->indexOf($cI) + 1 . "/" . $imgCount;?>
+              <?= $files->indexOf($file) + 1 . "/" . $fileCount;?>
             </p>
           </figcaption>
         </figure>
@@ -31,20 +41,22 @@
     </div>
   </div>
 
-  <div class="preview-image"><?= $imgs->first() ?></div>
+  <div class="preview-image"><?= $files->first() ?></div>
   
   <?php endif ?>
 
-  <?php foreach($article->documents() as $cD) : ?>
-    <div class="i i-3">
-      <a href="<?= $cD->url() ?>" target="_blank"><?= $cD->description() ?></a>
-    </div>
+  <?php $links = $article->links()->toStructure();
+        foreach ($links as $link) : ?>
+          <div class="i i-5">
+            <a href="<?= $link->target()->toLinkObject() ?>" target="_blank"><?= $link->title() ?></a>
+          </div>
   <?php endforeach ?>
 
-  <?php if($article->link()->isNotEmpty()) : ?>
-    <div class="i i-3">
-      <a href="<?= $article->link()->url() ?>" target="_blank">View documentation</a>
-    </div>
-  <?php endif ?>
+  <?php $documents = $article->docs()->toStructure();
+        foreach ($documents as $document) : ?>
+          <div class="i i-5">
+            <a href="<?= $document->target()->toFile()->url() ?>" target="_blank"><?= $document->title() ?></a>
+          </div>
+  <?php endforeach ?>
 
 </article>
