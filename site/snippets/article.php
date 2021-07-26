df91d20e4efa9b24417ce7d86535441143a42d2b
Oskar <oskarradonkimball@gmail.com>
2021-07-25 22:30:09 -0700

    remove gallery from article for testing

--- site/snippets/article.php
+++ site/snippets/article.php
@@ -1,6 +1,5 @@
 <article>
   <div class="i i-4" data-micromodal-trigger="<?= $article->uid() ?>">
-  <p>asdf</p>
     <!-- <a href="<?= $article->url() ?>"> -->
       <h3><?= $article->title() ?></h3>
       <p><?= $article->start()->toDate('F j, Y') ?> &ndash; <?= $article->end()->toDate('F j, Y') ?></p>
@@ -8,48 +7,7 @@
     <!-- </a> -->
   </div>
 
- <div class="modal micromodal-slide" id="<?= $article->uid() ?>" aria-hidden="true">
-    <div class="modal__overlay" tabindex="-1" data-micromodal-close>
-      <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="modal-1-title">
-        <button class="modal__close" aria-label="Close modal" data-micromodal-close></button>
-        <div class="modal__content" id="modal-1-content">
-          <?php 
-          $c = $article->files()->template("carousel");
-          if($c->isNotEmpty()) : 
-            $files = $c->sortBy('sort');
-            $fileCount = $files->count(); ?>
-            <!-- Slider main container -->
-            <div class="swiper-container">
-              <!-- Additional required wrapper -->
-              <div class="swiper-wrapper">
-                <!-- Slides -->
-                <?php 
-                foreach($files as $file) :
-                static $currentFileCount = 0;
-                        $currentFileCount++; ?>
-                  <figure class="swiper-slide">
-                    <?php if($file->type() == 'image') : ?>
-                      <img class="<?= $file->orientation() ?>" src="<?= $file->url() ?>" alt="" loading="lazy" />
-                    <?php elseif($file->type() == 'video') : ?>
-                      <video playsinline autoplay muted loop>
-                        <source src="<?= $file->url() ?>" type="<?= $file->mime() ?>">
-                      </video>
-                    <?php elseif($file->type() == 'audio') :  ?>
-                    <?php $audioId = $article->uid() . $currentFileCount; ?>
-                      <audio id="audio<?php echo $audioId ?>" controls src="<?= $file->url() ?>" type="<?= $file->mime() ?>"></audio>
-                    <?php endif ?>
-
-                    
-                </figure>
-              <?php endforeach ?>
-              </div>
-            </div>
-          <?php endif ?>
-
-        </div>
-      </div>
-    </div>
-  </div>
+ 
         
 
   
