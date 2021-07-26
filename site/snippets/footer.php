15319db38e2dc5912c959d20de49341bfe739144
Oskar <oskarradonkimball@gmail.com>
2021-07-25 18:01:49 -0700

    Create footer.php

---
+++ site/snippets/footer.php
@@ -0,0 +1,15 @@
+      <!-- Vendor JS -->
+      <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+      <script src="https://unpkg.com/micromodal/dist/micromodal.min.js"></script>
+
+      <!-- Author JS -->
+      <?= js('assets/js/site.js') ?>
+      <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+      <script>
+        window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+        ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
+      </script>
+      <script src="https://www.google-analytics.com/analytics.js" async></script>
+  </body>
+</html>
+
