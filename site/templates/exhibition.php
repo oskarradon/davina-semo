ce1c295eac51ed4adad40815cb9c711eab0edc0a
Oskar <oskarradonkimball@gmail.com>
2021-07-29 14:43:35 -0700

    Update exhibition.php

--- site/templates/exhibition.php
+++ site/templates/exhibition.php
@@ -1,32 +1,33 @@
 <?php snippet('header', ['template' => $page->id()]); ?>
 
 <body>
-<!-- <main id="exhibition"> -->
+
   <a href="/" id="close-button"><span>x</span></a>
+
   <?php 
     $c = $page->files()->template("carousel");
     if($c->isNotEmpty()) : 
       $files = $c->sortBy('sort');
       $fileCount = $files->count(); ?>
-      <!-- Slider main container -->
+
       <div class="swiper-container">
-        <!-- Additional required wrapper -->
         <div class="swiper-wrapper">
-          <!-- Slides -->
+
           <?php 
           foreach($files as $file) :
           static $currentFileCount = 0;
                   $currentFileCount++; ?>
+
             <figure class="swiper-slide" data-hash="<?= $file->hash() ?>">
+
               <?php if($file->type() == 'image') : ?>
                 <img class="<?= $file->orientation() ?>" src="<?= $file->url() ?>" alt="" loading="lazy" />
+
               <?php elseif($file->type() == 'video') : ?>
-                <video playsinline autoplay muted loop>
+                <video id="player" autoplay muted loop>
                   <source src="<?= $file->url() ?>" type="<?= $file->mime() ?>">
                 </video>
-              <?php elseif($file->type() == 'audio') :  ?>
-              <?php $audioId = $page->uid() . $currentFileCount; ?>
-                <audio id="audio<?php echo $audioId ?>" controls src="<?= $file->url() ?>" type="<?= $file->mime() ?>"></audio>
+
               <?php endif ?>
 
               <figcaption>
@@ -35,13 +36,16 @@
                 <p class="image-counter"><?= $files->indexOf($file) + 1 . "/" . $fileCount;?></p>
                 </div>
             </figcaption>
+
           </figure>
+
         <?php endforeach ?>
         </div>
+
         <div class="swiper-button-next"></div>
         <div class="swiper-button-prev"></div>
+
       </div>
     <?php endif ?>
-<!-- </main> -->
 
 <?php snippet('footer'); ?>
