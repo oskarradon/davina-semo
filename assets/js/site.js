2a9015dab8703af07b7be6ba7e3a8b157d66056b
Oskar <oskarradonkimball@gmail.com>
2021-07-29 12:32:42 -0700

    finish refactoring js and add close button conditional functionality

--- assets/js/site.js
+++ assets/js/site.js
@@ -12,6 +12,7 @@
 for (const collapsible of document.querySelectorAll(".collapsible")) {
   collapsible.addEventListener("click", function () {
     this.classList.toggle("active");
+    updateHash(collapsible);
     mainImageToggle();
   });
 }
@@ -54,7 +55,8 @@ initZoomSlider();
 
 function mainImageToggle() {
   let elements = document.getElementsByClassName("i-1"),
-    modelViewer = document.querySelector("model-viewer");
+    modelViewer = document.querySelector("model-viewer"),
+    mainImage = document.querySelector("div#main-image");
 
   if (modelViewer) {
     for (let i = 0; i < elements.length; i++) {
@@ -83,11 +85,8 @@ function mainImageToggle() {
 // READING AND WRITING URL
 // * * * * * * * * * * * *
 
-for (const collapsible of document.querySelectorAll(".collapsible")) {
-  collapsible.addEventListener("click", function () {
-    updateHash(collapsible);
-  });
-}
+// to preserve a user's expanded options in the file tree this function uses the history API to store
+// selected options as a long string of hashes using the data attribute anchor-id of each .collapsible element
 
 function updateHash(element) {
   let anchor = "#" + element.dataset.anchorId,
@@ -103,84 +102,28 @@ function updateHash(element) {
   }
 }
 
-// on page load
-// look at the URL of the page
-// divide the url into the "href"s of the options that are expanded,
-//   (probably by seperating between each slash character -- use a regex)
-// loop through each expandable tree option and if it's name === "href" found in URL ---> expand that option
+// on page load this function opens the expanded options and hides the main image/model viewer
 
 expandCollapsibleElements(window.location.hash);
 
 function expandCollapsibleElements(hash) {
   for (const collapsible of document.querySelectorAll(".collapsible")) {
-    let anchor = "#" + collapsible.dataset.anchorId,
-      mainImage = document.querySelector("div#main-image img"),
-      aside = document.getElementsByTagName("aside")[0];
+    let anchor = "#" + collapsible.dataset.anchorId;
     if (hash.includes(anchor)) {
       collapsible.classList.toggle("active");
-      mainImage.classList.add("hide");
-      aside.classList.add("hide");
+      mainImageToggle();
     }
   }
 }
 
-// Make the main image draggable
+// * * * * * * *
+// IMAGE GALLERY
+// * * * * * * *
 
-// dragElement(document.querySelectorAll("#main-image img")[0]);
-
-// function dragElement(elmnt) {
-//   var pos1 = 0,
-//     pos2 = 0,
-//     pos3 = 0,
-//     pos4 = 0;
-//   if (document.getElementById(elmnt.id + "header")) {
-//     // if present, the header is where you move the DIV from:
-//     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-//   } else {
-//     // otherwise, move the DIV from anywhere inside the DIV:
-//     elmnt.onmousedown = dragMouseDown;
-//   }
-
-//   function dragMouseDown(e) {
-//     e = e || window.event;
-//     e.preventDefault();
-//     // get the mouse cursor position at startup:
-//     pos3 = e.clientX;
-//     pos4 = e.clientY;
-//     document.onmouseup = closeDragElement;
-//     // call a function whenever the cursor moves:
-//     document.onmousemove = elementDrag;
-//   }
-
-//   function elementDrag(e) {
-//     e = e || window.event;
-//     e.preventDefault();
-//     // calculate the new cursor position:
-//     pos1 = pos3 - e.clientX;
-//     pos2 = pos4 - e.clientY;
-//     pos3 = e.clientX;
-//     pos4 = e.clientY;
-//     // set the element's new position:
-//     elmnt.style.top = elmnt.offsetTop - pos2 + "px";
-//     elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
-//   }
-
-//   function closeDragElement() {
-//     // stop moving when mouse button is released:
-//     document.onmouseup = null;
-//     document.onmousemove = null;
-//   }
-// }
-
-// * * * *
-// SWIPER
-// * * * *
+// Exhibition page uses Swiper library: https://swiperjs.com/
 
 const swiper = new Swiper(".swiper-container", {
   centeredSlides: true,
-  // hashNavigation: {
-  //   watchState: true,
-  // },
   keyboard: {
     enabled: true,
   },
@@ -190,3 +133,17 @@ const swiper = new Swiper(".swiper-container", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+// "close button" goes home by default, unless sent to exhibition page from home page
+
+if (
+  document.getElementById("close-button") &&
+  document.referrer.includes("davinasemo.com")
+) {
+  document
+    .getElementById("close-button")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      window.history.back();
+    });
+}
