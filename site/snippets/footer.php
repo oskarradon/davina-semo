87c34eca2479bf275a864931ccd0e63298ea1975
Oskar <oskarradonkimball@gmail.com>
2021-07-29 14:44:02 -0700

    rm google analytics and add plyr js CDN link

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,15 +1,10 @@
       <!-- Vendor JS -->
-      <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-      <script src="https://unpkg.com/micromodal/dist/micromodal.min.js"></script>
+      <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>   
+      <script src="https://cdn.plyr.io/3.6.8/plyr.js"></script>   
+      <?= js('assets/js/vendor/model-viewer.min.js', ['type' => 'module']) ?>
 
       <!-- Author JS -->
       <?= js('assets/js/site.js') ?>
-      <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
-      <script>
-        window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
-        ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
-      </script>
-      <script src="https://www.google-analytics.com/analytics.js" async></script>
   </body>
 </html>
 
